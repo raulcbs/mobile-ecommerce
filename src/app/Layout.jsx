dca49bd435188ffbaf2512333a8ab18433e3cb2d
raulcbs <raulcubas2016@gmail.com>
2025-04-12 19:42:07 +0100

    refactor(Layout): update layout structure and integrate icons

--- src/app/Layout.jsx
+++ src/app/Layout.jsx
@@ -1,17 +1,30 @@
 import React from "react"
 import { Outlet, Link } from "react-router"
+import { HomeIcon, ShoppingCardIcon } from "../views/shared/icons"
 
 export default function Layout() {
   return (
-    <div className="w-full h-screen flex flex-col bg-[#242424] text-[#ffffffde]">
-      <header className="px-8 py-4 bg-[#1a1a1a] flex items-center gap-4">
-        <h1>Mobile Ecommerce</h1>
-        <nav className="grow flex">
-          <Link to="/">Inicio</Link>
-        </nav>
-        <div>0</div>
-      </header>
-      <main className="w-full grow overflow-y-auto">
+    <div className="flex flex-col min-h-screen bg-white text-gray-800  dark:bg-gray-900 dark:text-white">
+      <nav className="bg-white dark:bg-gray-900 sticky w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <div className="max-w-7xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <HomeIcon />
+            <span className="hidden sm:inline-block self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              TecnoPocket
+            </span>
+          </a>
+
+          <div className="relative inline-flex items-center p-3 font-medium text-center text-white">
+            <ShoppingCardIcon />
+            <span className="sr-only">Notifications</span>
+            <div className="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white -top-2 -end-2 ">
+              0
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <main className="grow max-w-7xl mx-auto w-full">
         <Outlet />
       </main>
       <footer>
