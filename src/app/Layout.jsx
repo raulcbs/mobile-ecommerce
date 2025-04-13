823637cd01f8470f53515069801f3571b83b185a
raulcbs <raulcubas2016@gmail.com>
2025-04-13 21:27:25 +0100

    fix(Layout): update Header import path and add padding to main content

--- src/app/Layout.jsx
+++ src/app/Layout.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 import { Outlet } from "react-router"
-import { Header } from "../components/header/Header"
+import { Header } from "@/views/shared/components/Header"
 
 export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen bg-white text-gray-800  dark:bg-gray-900 dark:text-white">
       <Header />
 
-      <main className="grow container mx-auto px-8 md:px-4">
+      <main className="grow container mx-auto px-8 py-6 md:px-4">
         <Outlet />
       </main>
     </div>
