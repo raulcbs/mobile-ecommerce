71dd0e7657759e0e6c5ffdb084c2e26880e55774
raulcbs <raulcubas2016@gmail.com>
2025-04-13 20:45:42 +0100

    refactor(vite.config): enhance configuration by adding path aliasing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   base: "/",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   plugins: [react(), tailwindcss()],
   preview: {
     port: 3000,
@@ -15,4 +21,4 @@ export default defineConfig({
     host: true,
     origin: "http://0.0.0.0:3000",
   },
-});
+})
