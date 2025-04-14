aeec8c7307a0f5f8a68545d162203f3c090c9888
Raul Cubas <raulcubas2016@gmail.com>
2025-04-14 20:37:20 +0100

    refactor(ProductDetail): remove unused properties from product detail model

--- src/domain/adapters/adapt-product-detail.js
+++ src/domain/adapters/adapt-product-detail.js
@@ -8,45 +8,18 @@ export function adaptProductDetail(apiProduct) {
     price: apiProduct.price,
     imageUrl: apiProduct.imgUrl,
 
-    networkTechnology: apiProduct.networkTechnology,
-    networkSpeed: apiProduct.networkSpeed,
-    gprs: apiProduct.gprs,
-    edge: apiProduct.edge,
-    announced: apiProduct.announced,
-    status: apiProduct.status,
     dimentions: apiProduct.dimentions,
     weight: apiProduct.weight,
-    sim: apiProduct.sim,
-
     displayType: apiProduct.displayType,
-    displayResolution: apiProduct.displayResolution,
-    displaySize: apiProduct.displaySize,
-
     os: apiProduct.os,
     cpu: apiProduct.cpu,
-    chipset: apiProduct.chipset,
-    gpu: apiProduct.gpu,
-
-    externalMemory: apiProduct.externalMemory,
-    internalMemory: apiProduct.internalMemory,
     ram: apiProduct.ram,
-
     primaryCamera: apiProduct.primaryCamera,
     secondaryCamera: apiProduct.secondaryCmera,
-
-    speaker: apiProduct.speaker,
-    audioJack: apiProduct.audioJack,
-
-    wlan: apiProduct.wlan,
-    bluetooth: apiProduct.bluetooth,
-    gps: apiProduct.gps,
-    nfc: apiProduct.nfc,
-    radio: apiProduct.radio,
-    usb: apiProduct.usb,
-
-    sensors: apiProduct.sensors,
     battery: apiProduct.battery,
-    colors: apiProduct.colors,
-    options: apiProduct.options,
+
+    status: apiProduct.status,
+    colors: apiProduct.options.colors,
+    storages: apiProduct.options.storages,
   })
 }
--- src/domain/models/product-detail.js
+++ src/domain/models/product-detail.js
@@ -7,79 +7,33 @@ export class ProductDetail extends Product {
     model,
     price,
     imageUrl,
-    networkTechnology,
-    networkSpeed,
-    gprs,
-    edge,
-    announced,
-    status,
     dimentions,
     weight,
-    sim,
     displayType,
-    displayResolution,
-    displaySize,
     os,
     cpu,
-    chipset,
-    gpu,
-    externalMemory,
-    internalMemory,
     ram,
     primaryCamera,
     secondaryCamera,
-    speaker,
-    audioJack,
-    wlan,
-    bluetooth,
-    gps,
-    nfc,
-    radio,
-    usb,
-    sensors,
     battery,
     colors,
-    options,
+    storages,
+    status,
   }) {
     super({ id, brand, model, price, imageUrl })
 
-    this.networkTechnology = networkTechnology
-    this.networkSpeed = networkSpeed
-    this.gprs = gprs
-    this.edge = edge
-    this.announced = announced
-    this.status = status
     this.dimentions = dimentions
     this.weight = weight
-    this.sim = sim
     this.displayType = displayType
-    this.displayResolution = displayResolution
-    this.displaySize = displaySize
     this.os = os
     this.cpu = cpu
-    this.chipset = chipset
-    this.gpu = gpu
-    this.externalMemory = externalMemory
-    this.internalMemory = internalMemory
     this.ram = ram
     this.primaryCamera = primaryCamera
     this.secondaryCamera = secondaryCamera
-    this.speaker = speaker
-    this.audioJack = audioJack
-    this.wlan = wlan
-    this.bluetooth = bluetooth
-    this.gps = gps
-    this.nfc = nfc
-    this.radio = radio
-    this.usb = usb
-    this.sensors = sensors
     this.battery = battery
     this.colors = colors
-    this.options = options
-  }
-
-  get hasNfc() {
-    return !!this.nfc
+    this.storages = storages
+    this.status = status
   }
 
   get isAvailable() {
