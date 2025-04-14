92ade541ce8eb4a249196fefd3c8c4428b3e668d
Raul Cubas <raulcubas2016@gmail.com>
2025-04-14 21:27:16 +0100

    feat(Header): display cart count in header using cart context

--- src/views/shared/components/header.jsx
+++ src/views/shared/components/header.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router"
 import { HeroIcon, ShoppingCardIcon } from "@/views/shared/icons"
+import { useCart } from "@/views/shared/contexts/cart-context"
 
 export function Header() {
+  const { cartCount } = useCart()
+
   return (
     <nav className="shadow-sm bg-white dark:bg-gray-900 sticky w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="container flex flex-wrap items-center justify-between mx-auto py-4 px-8 md:px-4">
@@ -16,7 +19,7 @@ export function Header() {
           <ShoppingCardIcon />
           <span className="sr-only">Notifications</span>
           <div className="absolute inline-flex items-center justify-center w-5 h-5 text-sm font-bold -top-2 -end-2 ">
-            0
+            {cartCount}
           </div>
         </div>
       </div>
