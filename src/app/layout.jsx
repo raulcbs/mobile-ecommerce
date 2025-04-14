0ad1a54f3d9b0ec20133b92202bf0a7450c7d96d
Raul Cubas <raulcubas2016@gmail.com>
2025-04-14 21:24:41 +0100

    feat(Layout): wrap layout in CartProvider and add Toaster component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,19 @@
 import { Outlet } from "react-router"
 import { Header } from "@/views/shared/components/header"
+import { CartProvider } from "@/views/shared/contexts/cart-context"
+import { Toaster } from "sonner"
 
 export default function Layout() {
   return (
-    <div className="flex flex-col min-h-screen bg-white text-gray-800  dark:bg-gray-900 dark:text-white">
-      <Header />
+    <CartProvider>
+      <div className="flex flex-col min-h-screen bg-white text-gray-800  dark:bg-gray-900 dark:text-white">
+        <Header />
 
-      <main className="grow container mx-auto px-8 py-6 md:px-4">
-        <Outlet />
-      </main>
-    </div>
+        <main className="grow container mx-auto px-8 py-6 md:px-4">
+          <Outlet />
+        </main>
+        <Toaster richColors />
+      </div>
+    </CartProvider>
   )
 }
