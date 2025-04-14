9d2ac474ac45f249d8869137470d154fdedd1b46
Raul Cubas <raulcubas2016@gmail.com>
2025-04-14 22:02:42 +0100

    feat(Layout): wrap layout in ProductProvider for product state management

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,18 +2,21 @@ import { Outlet } from "react-router"
 import { Header } from "@/views/shared/components/header"
 import { CartProvider } from "@/views/shared/contexts/cart-context"
 import { Toaster } from "sonner"
+import { ProductProvider } from "@/views/shared/contexts/product-context"
 
 export default function Layout() {
   return (
     <CartProvider>
-      <div className="flex flex-col min-h-screen bg-white text-gray-800  dark:bg-gray-900 dark:text-white">
-        <Header />
+      <ProductProvider>
+        <div className="flex flex-col min-h-screen bg-white text-gray-800  dark:bg-gray-900 dark:text-white">
+          <Header />
 
-        <main className="grow container mx-auto px-8 py-6 md:px-4">
-          <Outlet />
-        </main>
-        <Toaster richColors />
-      </div>
+          <main className="grow container mx-auto px-8 py-6 md:px-4">
+            <Outlet />
+          </main>
+          <Toaster richColors />
+        </div>
+      </ProductProvider>
     </CartProvider>
   )
 }
